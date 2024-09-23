c7627319c52105bd28c2dd4ba21d879782dde2aa
Himanshu <Himanshu.H@deltaww.com>
2024-09-23 13:44:29 +0530

    docker container rename commands and add exe compatibility '_quote' and env var support

--- Docker.ps1
+++ Docker.ps1
@@ -1,27 +1,70 @@
 # Docker settings for linux-arm for DCU
 
-$IMAGE = "linux-arm"
-$CONTAINER = "linux-arm"
-$NETWORK = "linux-arm"
-$USER = "himanshu"
-$WSL_USER = "himanshu"
-$BASE_IMAGE = "osrf/ubuntu_armhf:focal"
-$DOCKER_IS_WSL_COMMAND = $false
-$DOCKER_USE_EXE = $false
+$IMAGE = $IMAGE ? $IMAGE : "linux-arm"
+$CONTAINER = $CONTAINER ? $CONTAINER : "linux-arm"
+$NETWORK = $NETWORK ? $NETWORK : "linux-arm"
+$USER = $USER ? $USER : "himanshu"
+$WSL_USER = $WSL_USER ? $WSL_USER : "himanshu"
+$BASE_IMAGE = $BASE_IMAGE ? $BASE_IMAGE : "osrf/ubuntu_armhf:focal"
 
-function docker-setup-linux-arm {
+$DOCKER_SHELL = $DOCKER_SHELL ? $DOCKER_SHELL : "zsh"
+$DOCKER_SAVE = $null -ne $DOCKER_SAVE ? $DOCKER_SAVE : $true
+
+$DOCKER_IS_WSL_COMMAND = $null -ne $DOCKER_IS_WSL_COMMAND ? $DOCKER_IS_WSL_COMMAND : $false
+$DOCKER_USE_EXE = $null -ne $DOCKER_USE_EXE ? $DOCKER_USE_EXE : $false
+
+function _quote {
+    param (
+        [string]$str
+    )
+
+    if ($DOCKER_IS_WSL_COMMAND -and $DOCKER_USE_EXE) {
+        return "'$str'"
+    } else {
+        return "$str"
+    }
+}
+
+function docker-env {
+    param (
+        [hashtable]$kvp = @{}
+        # [PSCustomObject]$kvp
+    )
+
+    docker version > $null
+
+    # foreach ($property in $kvp.PSObject.Properties) {
+    #     Set-Item -Path "Env:$($property.Name)" -Value $property.Value
+    #     $WSLENV += ':' + $property.Name
+    # }
+
+    $kvp.GetEnumerator() | ForEach-Object {
+        Set-Item -Path "Env:$($_.Key)" -Value $_.Value
+
+        if ($DOCKER_IS_WSL_COMMAND) {
+            $WSLENV = (Get-Item -Path "Env:WSLENV").Value
+            if (-not [string]::IsNullOrEmpty($WSLENV) -and $WSLENV[-1] -ne ":") {
+                $WSLENV += ":"
+            }
+            $WSLENV += $_.Key
+            Set-Item -Path "Env:WSLENV" -Value $WSLENV
+        }
+    }
+}
+
+function docker-setup-image {
     # basic setup
     if ( docker ps -a | Select-String $CONTAINER ) { docker stop $CONTAINER }
     docker pull $BASE_IMAGE
     docker run --rm -d -it --name $CONTAINER $BASE_IMAGE bash
     docker exec --user=root -it $CONTAINER useradd -mG 'adm,dialout,cdrom,floppy,sudo,audio,dip,video,plugdev' $USER
-    docker exec --user=root -it $CONTAINER bash -c 'apt update'
-    docker exec --user=root -it $CONTAINER bash -c 'apt install git zsh nano vim build-essential gcc g++ gdb -y'
+    docker exec --user=root -it $CONTAINER bash -c (_quote 'apt update')
+    docker exec --user=root -it $CONTAINER bash -c (_quote 'apt install git zsh nano vim build-essential gcc g++ gdb -y')
     docker exec --user=$USER -it $CONTAINER git clone https://github.com/htanwar922/.zsh.git /home/$USER/.zsh
     # docker exec --user=$USER -it $CONTAINER git clone https://github.com/zsh-users/zsh-autosuggestions.git /home/$USER/.zsh/zsh-autosuggestions
     # docker exec --user=$USER -it $CONTAINER git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/$USER/.zsh/zsh-syntax-highlighting
-    docker exec --user=root -it $CONTAINER bash -c 'apt-get install zsh-* -y'
-    docker exec --user=root -it $CONTAINER bash -c 'apt install zsh-autosuggestions zsh-syntax-highlighting -y'
+    docker exec --user=root -it $CONTAINER bash -c (_quote 'apt-get install zsh-* -y')
+    docker exec --user=root -it $CONTAINER bash -c (_quote 'apt install zsh-autosuggestions zsh-syntax-highlighting -y')
     docker exec --user=root -it $CONTAINER ln -s /home/$USER/.zsh/zshrc /root/.zshrc
     docker exec --user=root -it $CONTAINER ln -s /home/$USER/.zsh/zprofile /root/.zprofile
     docker exec --user=root -it $CONTAINER chsh -s /bin/zsh
@@ -29,7 +72,7 @@ function docker-setup-linux-arm {
     docker exec --user=$USER -it $CONTAINER ln -s /home/$USER/.zsh/zprofile /home/$USER/.zprofile
     docker exec --user=root -it $CONTAINER chsh -s /bin/zsh $USER
     if ($DOCKER_IS_WSL_COMMAND) {
-        docker exec --user=root -it $CONTAINER zsh -c "echo $USER ALL=\(ALL\) NOPASSWD:ALL | tee -a /etc/sudoers"
+        docker exec --user=root -it $CONTAINER zsh -c (_quote "echo $USER ALL=\(ALL\) NOPASSWD:ALL | tee -a /etc/sudoers")
     } else {
         docker exec --user=root -it $CONTAINER zsh -c "echo '$USER' ALL='(ALL)' NOPASSWD:ALL | tee -a /etc/sudoers"
     }
@@ -37,32 +80,33 @@ function docker-setup-linux-arm {
     docker stop $CONTAINER
     Write-Debug "Setup complete"
 }
-function docker-start-linux-arm {
+function docker-start-container {
+    param (
+        [string]$cmd = ""
+    )
+
     docker network create $NETWORK
     if ( docker ps -a | Select-String $CONTAINER ) {
         docker stop $CONTAINER
     }
 
     if ($DOCKER_IS_WSL_COMMAND) {
-        if ($DOCKER_USE_EXE) {
-            $cmd = "`'`"cd; zsh -ils`"`'"
-        } else {
-            $cmd = 'cd; zsh -ils'
-        }
         if ( "$args" -eq "" ) {
             docker run --rm -d -it --privileged --cap-add=SYS_PTRACE `
                 --security-opt seccomp=unconfined --security-opt apparmor=unconfined `
                 --network $NETWORK -p 58020:58020 -p 58021:58021 -p 9020:9020/udp `
+                ($env:WSLENV -split ':' | ForEach-Object { $_ ? "-e $_" : $null }) `
                 -v /home/$WSL_USER/.ssh:/home/$USER/.ssh `
                 -v /home/$WSL_USER/concentrator/:/home/$USER/concentrator `
-                --name $CONTAINER --user=$USER $IMAGE zsh -ilsc $cmd
+                --name $CONTAINER --user=$USER $IMAGE
         } else {
             docker run --rm -d -it --privileged --cap-add=SYS_PTRACE `
                 --security-opt seccomp=unconfined --security-opt apparmor=unconfined `
                 --network $NETWORK "$args" `
+                ($env:WSLENV -split ':' | ForEach-Object { $_ ? "-e $_" : $null }) `
                 -v /home/$WSL_USER/.ssh:/home/$USER/.ssh `
                 -v /home/$WSL_USER/concentrator/:/home/$USER/concentrator `
-                --name $CONTAINER --user=$USER $IMAGE zsh -ilsc $cmd
+                --name $CONTAINER --user=$USER $IMAGE
         }
     } else {
         if ( "$args" -eq "" ) {
@@ -71,31 +115,40 @@ function docker-start-linux-arm {
             --network $NETWORK -p 58020:58020 -p 58021:58021 -p 9020:9020/udp `
             -v \\wsl.localhost\Ubuntu\home\$WSL_USER\.ssh:/home/$USER/.ssh `
             -v \\wsl.localhost\Ubuntu\home\$WSL_USER\concentrator\:/home/$USER/concentrator `
-            --name $CONTAINER --user=$USER $IMAGE zsh -ilsc 'cd; zsh -ils'
+            --name $CONTAINER --user=$USER $IMAGE
         } else {
             docker run --rm -d -it --privileged --cap-add=SYS_PTRACE `
                 --security-opt seccomp=unconfined --security-opt apparmor=unconfined `
                 --network $NETWORK "$args" `
                 -v \\wsl.localhost\Ubuntu\home\$WSL_USER\.ssh:/home/$USER/.ssh `
                 -v \\wsl.localhost\Ubuntu\home\$WSL_USER\concentrator\:/home/$USER/concentrator `
-                --name $CONTAINER --user=$USER $IMAGE zsh -ilsc 'cd; zsh -ils'
+                --name $CONTAINER --user=$USER $IMAGE
         }
     }
 }
-function docker-run-linux-arm {
+function docker-run-container {
+    param (
+        [string]$cmd = ""
+    )
+
     docker version > $null
-    if ($DOCKER_USE_EXE) {
-        docker exec --user=$USER -it $CONTAINER zsh -ilsc `'"cd; zsh -ils"`'
-        Write-Debug 'Using WSL from docker.exe...'
-    } else {
-        docker exec --user=$USER -it $CONTAINER zsh -ilsc 'cd; zsh -ils'
+    if ($cmd -eq "") {
+        if ($DOCKER_USE_EXE) {
+            Write-Debug 'Using WSL from docker.exe...'
+            $cmd = "$DOCKER_SHELL -ilsc (_quote 'cd; $DOCKER_SHELL -ils')"
+        } else {
+            $cmd = "$DOCKER_SHELL -ilsc 'cd; $DOCKER_SHELL -ils'"
+        }
     }
-    docker-save-linux-arm
+    docker exec --user=$USER -it $CONTAINER $cmd
+    docker-save-container
 }
-function docker-save-linux-arm {
-    docker commit $CONTAINER $IMAGE
+function docker-save-container {
+    if ($DOCKER_SAVE) {
+        docker commit $CONTAINER $IMAGE
+    }
 }
-function docker-cleanup-linux-arm {
+function docker-cleanup-container {
     docker stop $CONTAINER
     docker network rm $NETWORK
 
@@ -104,9 +157,9 @@ function docker-cleanup-linux-arm {
         | ConvertFrom-Csv -Header "REPOSITORY", "TAG", "IMAGE_ID", "CREATED", "SIZE" `
         | Select-Object -ExpandProperty IMAGE_ID | ForEach-Object { docker rmi $_ --force }
 }
-function docker-stop-linux-arm {
-    docker-save-linux-arm
-    docker-cleanup-linux-arm
+function docker-stop-container {
+    docker-save-container
+    docker-cleanup-container
 }
 
 function docker {
