0143dac687cc132e4267320667d1ba72b79cdce8
Himanshu <Himanshu.H@deltaww.com>
2024-10-28 11:32:37 +0530

    visual studio shell

---
+++ VisualStudio.ps1
@@ -0,0 +1,6 @@
+# VS 2022
+function vs2022 {
+    # Import-Module 'C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\Microsoft.VisualStudio.DevShell.dll'
+    # Enter-VsDevShell 5d1c6581
+	&'C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\Launch-VsDevShell.ps1'
+}
